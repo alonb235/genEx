71cd006f5f11327d071d0c8b05c8755d4ef1952a
dylanscheidegg <dylan.scheidegg123@gmail.com>
2024-04-30 19:35:10 +0000

    WORKING

--- genEx/src/app/app.module.ts
+++ genEx/src/app/app.module.ts
@@ -5,8 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChatBubbleComponent } from 'components/chat-bubble/chat-bubble.component';
+import { RetirementContributionSliderComponent } from 'components/retirement-contribution-slider/retirement-contribution-slider.component';
+import { YourIraComponent } from 'components/your-ira/your-ira.component';
 
 @NgModule({
+    declarations: [
+        AppComponent,
+        ChatBubbleComponent,
+        RetirementContributionSliderComponent,
+        YourIraComponent
+    ],
     imports: [
         BrowserModule,
         HttpClientModule,
