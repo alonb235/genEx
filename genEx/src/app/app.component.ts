9b800349b8f47e739534627ff9b511405606658e
dylanscheidegg <dylan.scheidegg123@gmail.com>
2024-04-30 15:23:43 +0000

    Changes

--- genEx/src/app/app.component.ts
+++ genEx/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { YourIraComponent } from './components/your-ira/your-ira.component';
 import { RetirementContributionSliderComponent } from './components/retirement-contribution-slider/retirement-contribution-slider.component';
-import { ChatBubbleComponent } from 'components/chat-bubble/chat-bubble.component';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +10,6 @@ import { ChatBubbleComponent } from 'components/chat-bubble/chat-bubble.componen
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-
-  title= "Hello"
-  ngOnInit() {
-
-  }
+export class AppComponent {
+  return
 }
