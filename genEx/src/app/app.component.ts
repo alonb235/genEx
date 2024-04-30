05c8dedee4cb6a04fe9908a552eb31bdd277c862
dylanscheidegg <dylan.scheidegg123@gmail.com>
2024-04-30 13:55:07 +0000

    HHHH

--- genEx/src/app/app.component.ts
+++ genEx/src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RetirementContributionSliderComponent } from './components/retirement-contribution-slider/retirement-contribution-slider.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RetirementContributionSliderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
