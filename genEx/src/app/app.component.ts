fd82628220b5bbeb7b43167bde1a35dd2190bfcc
dylanscheidegg <dylan.scheidegg123@gmail.com>
2024-05-01 17:33:27 +0000

    NIce

--- genEx/src/app/app.component.ts
+++ genEx/src/app/app.component.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { YourIraComponent } from './components/your-ira/your-ira.component';
 import { CommonModule } from '@angular/common';
-import { RetirementContributionSliderComponent } from './components/retirement-contribution-slider/retirement-contribution-slider.component';
-import { ChatBubbleComponent } from 'components/chat-bubble/chat-bubble.component';
-import { ChatBoxComponent } from 'components/chat-box/chat-box.component';
 import { MainPageComponent } from 'components/main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, YourIraComponent, RetirementContributionSliderComponent, ChatBubbleComponent, ChatBoxComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  chatVisible: boolean = false;
 
-  openChat() {
-    this.chatVisible = !this.chatVisible;
-  }
 }
--- genEx/src/app/components/main-page/main-page.component.ts
+++ genEx/src/app/components/main-page/main-page.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HandlerService } from 'services/handler.service';
+import { YourIraComponent } from '../your-ira/your-ira.component';
+import { CommonModule } from '@angular/common';
+import { RetirementContributionSliderComponent } from '../retirement-contribution-slider/retirement-contribution-slider.component';
+import { ChatBubbleComponent } from '../chat-bubble/chat-bubble.component';
+import { ChatBoxComponent } from '../chat-box/chat-box.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, YourIraComponent, RetirementContributionSliderComponent, ChatBubbleComponent, ChatBoxComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-  componentsToDisplay = []
+  componentsToDisplay = []  
+  chatVisible: boolean = false;
 
   constructor(private readonly handlerService: HandlerService) {
     this.getComponents()
@@ -19,5 +24,9 @@ export class MainPageComponent {
   async getComponents() {
     await this.handlerService.postUserRequest('recently this user checked their account balance and contributes to their ira every month. display components based off of user history', 0)
     this.componentsToDisplay = this.handlerService.getComponentsToRender()
+  }
+
+  openChat() {
+    this.chatVisible = !this.chatVisible;
   }
 }
